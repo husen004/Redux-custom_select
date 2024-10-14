28ddc82e55d7bb629ce3f9e740512deab2765277
Ikary <husenikary@mail.ru>
2024-10-14 10:01:06 +0500

    fetcjing data

--- src/store/user/user.actions.js
+++ src/store/user/user.actions.js
@@ -1,8 +1,6 @@
 import { createAsyncThunk } from "@reduxjs/toolkit"
-import { useId } from "react"
 
 const fetchUserById = userId => new Promise(resolve => setTimeout(() => resolve({ id: 1, name: 'Max' }), 1000))
-
 
 export const getUserById = createAsyncThunk('users/by-id', async(userID, thunkApi) => {
     const response = await fetchUserById(userId)
