3b29f58a2b8ad28cef24e77fdbc11e7213b8b819
Ikary <husenikary@mail.ru>
2024-10-23 21:44:18 +0500

    Add

--- src/store/user/use.slice.js
+++ src/store/user/use.slice.js
@@ -1,10 +1,16 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { getUserById } from "./user.actions";
 
 
 export const useSlice = createSlice({
     name:'users',
     initialState: [],
     reducers: {
-        
+        isLoading: false,
+    },
+    extraReducers: builder => {
+        builder.addCase(getUserById.pending, state => {
+
+        })
     }
 })
--- src/store/user/user.actions.js
+++ src/store/user/user.actions.js
@@ -2,7 +2,11 @@ import { createAsyncThunk } from "@reduxjs/toolkit"
 
 const fetchUserById = userId => new Promise(resolve => setTimeout(() => resolve({ id: 1, name: 'Max' }), 1000))
 
-export const getUserById = createAsyncThunk('users/by-id', async(userID, thunkApi) => {
-    const response = await fetchUserById(userId)
-    return response
+export const getUserById = createAsyncThunk('users/by-id', async(userId, thunkApi) => {
+    try {
+        const response = await fetchUserById(userId)
+        return response
+    } catch (error) {
+        thunkApi.rejectWithValue({})
+    }
 })
