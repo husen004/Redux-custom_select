463e30d3375f756626a6b9bb03205178708438cc
Ikary <husenikary@mail.ru>
2024-10-25 21:15:36 +0500

    Heard Changes

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,7 +5,7 @@ import { useSelector } from 'react-redux';
 export const Header = () => {
     const { favorites } = useSelector(state => state)
   return (
-    <header className='cursor-pointer flex justify-end p-14 relative' >
+    <header className='flex justify-end p-14 relative' >
      <FaHeart fontSize={50}/>
      <span className={`absolute bottom-[60px] text-center h-[24px] w-[20px] rounded-full bg-green-300 ${favorites.length === 0 ? "hidden" : "block"}`}>{favorites.length}</span>
     </header>
