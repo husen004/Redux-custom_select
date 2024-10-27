d2d112c0ef1b82c2e6ed86b840610ec22c9dc3ca
Ikary <husenikary@mail.ru>
2024-10-27 15:53:25 +0500

    store changes

--- src/store/store.jsx
+++ src/store/store.jsx
@@ -2,6 +2,7 @@ import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { reducer as favoritesReducer } from "./favorites/favorites.slice";
 import { userSlice } from "./user/use.slice";
 
+
 const reducers = combineReducers({
     favorites: favoritesReducer,
     user: userSlice.reducer,
