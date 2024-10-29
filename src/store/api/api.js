f8ef16943a5583417b7288508ec00320df4f052b
Ikary <husenikary@mail.ru>
2024-10-29 18:32:36 +0500

    end point

--- src/store/api/api.js
+++ src/store/api/api.js
@@ -4,10 +4,25 @@ const API_URL = "http://localhost:3000/recipes"
 
 export const api = createApi({
     reducerPath: 'api',
+    tagTypes: ['Recipe'],
     baseQuery: fetchBaseQuery({
         baseUrl: API_URL,
-        
-    })
+    }),
+    endpoints: builder => ({
+        getRecipe: builder.query({
+            query: () => '/',
+        }),
+        createRecipe: builder.mutation({
+            query: (recipe) => ({
+                body: (recipe) => ({
+                    body: recipe,
+                    url: '/',
+                    method: 'POST',
+                })
+            }),
+        }),
+    }),
+    
 })
   
 
