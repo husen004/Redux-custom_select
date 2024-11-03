16c8a38165772399ce9bd41f7bf829665cfa5629
Ikary <husenikary@mail.ru>
2024-11-03 15:39:33 +0500

    Text Changes

--- src/components/create-recipe/CreateRecipe.jsx
+++ src/components/create-recipe/CreateRecipe.jsx
@@ -24,7 +24,7 @@ export default function CreateRecipe() {
             <h1>Create New Recipe:</h1>
          <form className="gap-4" onSubmit={handleSubmit}>
             <input type="text" placeholder="Recipe Name" value={recipe.name} onChange={e => setRecipe({...recipe, name: e.target.value})} className="input "/>
-            <input type="text" placeholder="Recipe Image URL" value={recipe.image} onChange={e => setRecipe({...recipe, image: e.target.value})} className="input"/>
+            <input type="text" placeholder="Recipe Image Address" value={recipe.image} onChange={e => setRecipe({...recipe, image: e.target.value})} className="input"/>
             <button type="submit" className="border-2 rounded-2xl p-2 bg-green-300 border-green-400 mb-6 ml-2">Create Recipe</button>
          </form>
 
