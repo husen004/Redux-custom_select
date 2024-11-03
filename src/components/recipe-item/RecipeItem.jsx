2e661c5c214909b6b502bdfa6792245a8a647faa
Ikary <husenikary@mail.ru>
2024-11-03 13:33:13 +0500

    images from api added

--- src/components/recipe-item/RecipeItem.jsx
+++ src/components/recipe-item/RecipeItem.jsx
@@ -14,7 +14,7 @@ function RecipeItem({recipe}) {
   return (  
     <div className=''>
     <div className='bg-white my-6 text-center rounded-2xl p-6'>
-      {/* <img src={recipe.image} alt={recipe.name} className='h-32 w-32 object-cover' /> */}
+      <img src={recipe.image} alt={recipe.name} className='w-[300px] h-[250px] object-cover' />
       <h1 className='my-4'>{recipe.name}</h1>
       <button onClick={() => toggleFavorites(recipe)} className={`w-[170px] rounded-xl border-2 p-2 transition-all ease-in-out duration-400 hover:scale-105 cursor-pointer ${isExist ? "border-red-400 bg-red-300" : "border-green-400 bg-green-300"} `} type='button'>{isExist ? "Remove from" : "Add to"} favorites</button>
     </div>
